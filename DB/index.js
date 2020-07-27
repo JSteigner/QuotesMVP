d2dd33df9707b276e5c8ee3e0cb0bbcde8e15b8a
Jay Steigner <ioctosteigner@gmail.com>
2020-07-27 16:11:08 -0500

    setting app up from scratch

---
+++ DB/index.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'quotes'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log('error connecting to DB');
+  }
+  console.log('DB connected');
+})
