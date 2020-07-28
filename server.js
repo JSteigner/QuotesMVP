16a9fdcd2977bcbc3c43a6f97e0be7c691399fef
Jay Steigner <ioctosteigner@gmail.com>
2020-07-27 19:20:45 -0500

    webpack working

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const { db } = require('./DB/index');
 const express = require('express');
 const app = express();
 
-const PORT = 8080;
-
-app.get('/', (req, res) => res.sendFile(`${__dirname}/index.html`));
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`server is listening on ${PORT}`)
